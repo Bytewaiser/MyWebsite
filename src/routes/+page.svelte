98341c174d9f01c2e5cd91a702041b3f7dd1a615
Bytewaiser <bytewaiser@gmail.com>
2023-03-22 01:10:54 +0300

    Early Stop

---
+++ src/routes/+page.svelte
@@ -0,0 +1,63 @@
+<script>
+    import avatar from "$lib/images/avatar.jpg";
+    import TwitterIcon from "../components/icons/TwitterIcon.svelte";
+    import GithubIcon from "../components/icons/GithubIcon.svelte";
+    import LinkedinIcon from "../components/icons/LinkedinIcon.svelte";
+</script>
+
+<svelte:head>
+    <title>Hasan Hüseyin Yurdagül - Home</title>
+    <meta
+        name="description"
+        content="Hasan Hüseyin Yurdagül Personal Website"
+    />
+</svelte:head>
+
+<section>
+    <img src={avatar} alt="Photo of Hasan Hüseyin Yurdagül" />
+    <h1>Hasan Hüseyin Yurdagül</h1>
+    <p>
+        Hi, I am Hasan, I am a <b>Senior Machine Learning Engineer</b> at
+        <a href="https://uni-yaz.com" target="_blank">Universal Software</a> and
+        <a href="https://smartiks.com.tr" target="_blank">Smartiks Software</a>.
+    </p>
+    <div class="contact">
+        <p>Reach me at:</p>
+        <div>
+            <a href="https://www.linkedin.com/in/bytewaiser/" target="_blank">
+                <LinkedinIcon />
+            </a>
+            <a href="https://github.com/bytewaiser" target="_blank">
+                <GithubIcon />
+            </a>
+            <a href="https://twitter.com/hasanhyurdagul" target="_blank">
+                <TwitterIcon />
+            </a>
+        </div>
+    </div>
+</section>
+
+<style>
+    .contact {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
+    .contact a {
+        margin-left: 5px;
+        margin-right: 5px;
+    }
+    img {
+        width: 100px;
+        border-radius: 50%;
+        margin-bottom: -20px;
+    }
+    section {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        flex: 0.5;
+    }
+</style>
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,117 @@
+<script>
+    import TimelineItem from "../../components/TimelineItem.svelte";
+</script>
+
+<svelte:head>
+    <title>About - Hasan Hüseyin Yurdagül</title>
+    <meta name="description" content="About Hasan Hüseyin Yurdagül" />
+</svelte:head>
+
+<style>
+h2 {
+    margin-bottom: 1rem;
+    font-size: x-large; 
+    font-weight: bold;
+}
+
+</style>
+
+<div class="text-column">
+    <h1>About Me</h1>
+    <section aria-label="Timeline list">
+        <h2>Work Timeline</h2>
+        <p>This is my work history, some of them I still work some of them not. </p>
+        <ul>
+            <TimelineItem
+                time="2022-01"
+                title="Datakod"
+                position="R&D Center Consultant"
+                href="https://www.datakod.com/"
+            />
+            <TimelineItem
+                time="2022-01"
+                title="Kolaybi"
+                position="R&D Center Consultant"
+                href="https://www.kolaybi.com/"
+            />
+            <TimelineItem
+                time="2022-01"
+                title="NanoWorks"
+                position="R&D Center Consultant"
+                href="https://nano.eu.com/"
+            />
+            <TimelineItem
+                time="2021-10"
+                title="Synapsis"
+                position="Data Science Consultant"
+                href="https://synapsis.com.tr/"
+            />
+            <TimelineItem
+                time="2021-09"
+                title="UGM"
+                position="Lead Machine Learning Engineer"
+                href="https://ugm.com.tr"
+            />
+            <TimelineItem
+                time="2021-09"
+                title="HepsiEmlak"
+                position="R&D Center Consultant"
+                href="https://www.hepsiemlak.com/"
+            />
+            <TimelineItem
+                time="2021-08"
+                title="Protel"
+                position="Data Science Consultant"
+                href="https://www.protel.com.tr/"
+            />
+            <TimelineItem
+                time="2021-05"
+                title="OxyAI"
+                position="Lead Machine Learning Engineer"
+                href="http://www.oxyai.com"
+            />
+            <TimelineItem
+                time="2021-04"
+                title="Universal"
+                position="Lead Research Scientist"
+                href="https://www.uni-yaz.com"
+            />
+            <TimelineItem
+                time="2021-04"
+                title="Albert Solino"
+                position="R&D Center Consultant"
+                href="https://www.albertsolino.com/"
+            />
+            <TimelineItem
+                time="2020-12"
+                title="Smartiks"
+                position="Data Science Consultant"
+                href="https://www.smartiks.com.tr/"
+            />
+            <TimelineItem
+                time="2020-08"
+                title="Technorot"
+                position="Data Science Consultant"
+                href="https://www.teknorot.com/"
+            />
+            <TimelineItem
+                time="2020-08"
+                title="Sefamerve"
+                position="Data Science Consultant"
+                href="https://www.sefamerve.com/"
+            />
+            <TimelineItem
+                time="2020-05"
+                title="Comdata"
+                position="R&D Center Consultant"
+                href="https://www.comdatagroup.com/"
+            />
+            <TimelineItem
+                time="2020-05"
+                title="Gtech"
+                position="Data Science Consultant"
+                href="https://www.gtech.com.tr/en/"
+            />
+        </ul>
+    </section>
+</div>
