2385ab6c77586ba2e00827eb4b8f617d6eae6589
Bytewaiser <bytewaiser@gmail.com>
2023-03-22 01:11:10 +0300

    Early Stop

---
+++ src/components/TimelineItem.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+    export let time: string;
+    export let title: string;
+    export let position: string;
+    export let href: string;
+</script>
+
+<li>
+    <time>{time}</time>
+    <a {href} target="_blank">{title}</a> - {position}
+</li>
+
+<style>
+    li {
+        display: flex;
+        flex-wrap: wrap;
+        align-items: center;
+        column-gap: 0.5rem;
+        margin-bottom: 0.5rem;
+    }
+
+    time {
+        min-width: 75px;
+        color: rgb(107 114 128);
+    }
+
+    a {
+        display: inline-block;
+    }
+</style>
